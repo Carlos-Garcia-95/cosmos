8a42a3a2b1c01247fb28e37d65eeece729b7c5bd
Carlos-Garcia-95 <cozar1995@hotmail.com>
2025-04-14 13:34:56 +0200

    no message

--- app/Http/Controllers/Controller_LOCAL_513.php
+++ app/Http/Controllers/Controller_LOCAL_513.php
@@ -5,14 +5,10 @@ namespace App\Http\Controllers;
 abstract class Controller
 {
 <<<<<<< HEAD
-<<<<<<< HEAD
     // CPrueba de merge
     // Cambios de nuevooo golhoasdawd   
     // no se que estoy haciendo
 =======
     // Carlos la lia en la empresa
 >>>>>>> eeedb25 (se añaden comentarios)
-=======
-    //
->>>>>>> c853158 (Revert "se añaden comentarios")
 }
