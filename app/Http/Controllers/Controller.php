a0358806ab1301bad806448575d19fba9b038f2d
Carlos-Garcia-95 <cozar1995@hotmail.com>
2025-04-14 13:33:58 +0200

    añadido login

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,7 +4,11 @@ namespace App\Http\Controllers;
 
 abstract class Controller
 {
+<<<<<<< HEAD
     // CPrueba de merge
     // Cambios de nuevooo golhoasdawd   
     // no se que estoy haciendo
+=======
+    // Carlos la lia en la empresa
+>>>>>>> eeedb25 (se añaden comentarios)
 }
